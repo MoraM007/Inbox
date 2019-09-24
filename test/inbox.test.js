9668d435782a19cf47d997242642cfc341341421
MoraM007 <mostafamorsy466@gmail.com>
2019-09-24 02:11:37 +0200

    //v1.2 testing

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,34 +2,17 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+beforeEach(() =>{
+    //Get A list of All Accounts
+    web3.eth.getAccounts().then(fetchedAccounts => {
+         console.log(fetchedAccounts);
+      });
 
-class Car{
-    park(){
-        return 'stopped';
-    }
-    drive(){
-        return 'vroom';
-    }
-}
-
-let car;
-
-beforeEach(() => {
-
-    car = new Car();
-    
+    //Use one of those accounts to deploy
+    //the contract
 });
 
-describe('Car', () => {
-
-    it('has Park Function',() =>{
-
-        assert.equal(car.park(), 'stopped');
-    });
-
-    it('can drive', () => {
-        
-        assert.equal(car.drive(), 'vroom');
-    });
-
+describe('Inbox', () =>{
+    it('deploys a contract', () => {});
 });
+
